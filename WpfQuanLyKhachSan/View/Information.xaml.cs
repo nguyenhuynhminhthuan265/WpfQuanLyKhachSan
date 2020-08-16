25ce30c9205893c5c5610b5ea8c5d5ab2bc57755
Le Thanh <56119673+lethanh050899@users.noreply.github.com>
2020-08-16 21:06:58 +0700

    Add View/Information

---
+++ WpfQuanLyKhachSan/View/Information.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfQuanLyKhachSan.View
+{
+    /// <summary>
+    /// Interaction logic for Information.xaml
+    /// </summary>
+    public partial class Information : Page
+    {
+        Frame MyFrame;
+        public Information()
+        {
+            InitializeComponent();
+        }
+
+        public Information(Frame frame)
+        {
+            this.MyFrame = frame;
+            InitializeComponent();
+        }
+
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
