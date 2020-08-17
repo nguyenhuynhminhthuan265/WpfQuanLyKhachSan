aad527cf348c2b93dd75c1af6d8ca62454c9e6f7
Le Thanh <56119673+lethanh050899@users.noreply.github.com>
2020-08-17 10:01:13 +0700

    test branch

---
+++ WpfQuanLyKhachSan/Migrations/202008170256154_create_db.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfQuanLyKhachSan.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class create_db : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.TypeRooms", "Price", c => c.Double(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.TypeRooms", "Price", c => c.Single(nullable: false));
+        }
+    }
+}
